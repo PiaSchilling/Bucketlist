10a0b7faafba642106ed94d3cb4ef849259c9d4b
Pia Schilling <ps149@hdm-stuttgart.de>
2021-05-18 18:37:45 +0200

    Beschreibungen zu Category-Enum-Elementen hinzugefügt

--- src/main/java/core/Category.java
+++ src/main/java/core/Category.java
@@ -1,6 +1,28 @@
 package core;
 
+/**
+ * predefined Categories that can be chosen when a new Event is added
+ * Every Category has a short description about what kind of event might fit to it
+ */
 public enum Category {
-    SKILLS,TRAVEL,CULTURE,SHOPPING,LIFEGOALS,CULINARY, EDUCATION, SPORT, HOBBY,FAMILY,RELATIONSHIP, FRIENDS;
+
+    SKILLS("Things you want to learn"),
+    TRAVEL("Places you want to visit"),
+    CULTURE("Cultural events/places you want to visit"),
+    SHOPPING("Things you want to buy"),
+    LIFEGOALS("Goals you want to achieve in your life"),
+    CULINARY("Things you want to eat/drink"),
+    EDUCATION("Knowledge you want to earn"),
+    SPORT("Sporty things you want to do"),
+    HOBBY("Hobbies you want to try"),
+    FAMILY("Things you want to do with you family"),
+    RELATIONSHIP("Things you want to do with your partner"),
+    FRIENDS("Things you want to do with your friends");
+
+    String description;
+
+    Category(String description){
+        this.description = description;
+    }
 
 }
