2c543c6363a0b4d7a2201c4ece1eff22b717750a
Merve <mo064@hmd-stuttgart.de>
2021-06-12 21:47:13 +0200

    Unit Test Package "Model" angefangen

---
+++ src/test/java/de/hdmstuttgart/mi/bucketlist/Model/EventTest.java
@@ -0,0 +1,30 @@
+package de.hdmstuttgart.mi.bucketlist.Model;
+
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    @Test
+    void completeEvent() {
+    }
+
+    @Test
+    void closingDate() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getEventName() {
+    }
+
+    @Test
+    void getEventCategory() {
+    }
+
+    @Test
+    void getIsCompleted() {
+    }
+}
---
+++ src/test/java/de/hdmstuttgart/mi/bucketlist/Model/EventlistTest.java
@@ -0,0 +1,42 @@
+package de.hdmstuttgart.mi.bucketlist.Model;
+
+import org.junit.jupiter.api.Test;
+
+class EventlistTest {
+
+    @Test
+    void addListener() {
+    }
+
+    @Test
+    void addEvent() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+
+    @Test
+    void completeEvent() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getExpiryDateGregorian() {
+    }
+
+    @Test
+    void getEvents() {
+    }
+
+    @Test
+    void getEventByName() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
