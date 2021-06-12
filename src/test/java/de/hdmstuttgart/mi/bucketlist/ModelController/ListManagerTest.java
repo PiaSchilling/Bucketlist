e540411c4e63b8e19d2ba20dacc064515f09730b
Sara Tietze <st093@hdm-stuttgart.de>
2021-06-12 15:44:30 +0200

    Tests Package "ViewController" und "ModelController"

---
+++ src/test/java/de/hdmstuttgart/mi/bucketlist/ModelController/ListManagerTest.java
@@ -0,0 +1,121 @@
+package de.hdmstuttgart.mi.bucketlist.ModelController;
+
+import de.hdmstuttgart.mi.bucketlist.Model.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ListManagerTest {
+
+    ListManager listManager;
+
+
+    @BeforeEach
+    public void prepareDummyFiles(){
+        this. listManager= new ListManager();
+    }
+
+    @Test
+    @DisplayName("testCreateEventlist should work")
+    void testCreateEventlist() {
+        listManager.createEventlist("TestListA",18,8,2022);
+        listManager.createEventlist("TestListB");
+
+        ListManager compareListManager = new ListManager();
+        compareListManager.createEventlist("TestListA",18,8,2022);
+        compareListManager.createEventlist("TestListB");
+
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+    }
+
+
+    @Test
+    @DisplayName("testDeleteEventlist should work")
+    void testDeleteEventlist() {
+        listManager.createEventlist("TestListA",18,8,2022);
+        listManager.createEventlist("TestListB");
+        listManager.deleteEventlist("TestListB");
+
+        ListManager compareListManager = new ListManager();
+        compareListManager.createEventlist("TestListA",18,8,2022);
+
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+    }
+
+    @Test
+    @DisplayName("testAddEventToList should work")
+    void testAddEventToList() {
+        listManager.createEventlist("TestListA",18,8,2022);
+        listManager.createEventlist("TestListB");
+        listManager.addEventToList("TestEventA_A", Category.FAMILY,"TestListA");
+        listManager.addEventToList("TestEventB_A", Category.SKILLS,"TestListA");
+
+
+        ListManager compareListManager = new ListManager();
+        compareListManager.createEventlist("TestListA",18,8,2022);
+        compareListManager.createEventlist("TestListB");
+        compareListManager.addEventToList("TestEventA_A", Category.FAMILY,"TestListA");
+        compareListManager.addEventToList("TestEventB_A", Category.SKILLS,"TestListA");
+
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+
+
+        listManager.addEventToList("TestEventA_B", Category.SKILLS,"TestListB");
+        compareListManager.addEventToList("TestEventA_B", Category.SKILLS,"TestListB");
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+
+    }
+
+    @Test
+    @DisplayName("testDeleteEvent should work")
+    void testDeleteEvent() {
+        listManager.createEventlist("TestListA",18,8,2022);
+        listManager.createEventlist("TestListB");
+        listManager.addEventToList("TestEventA_A", Category.FAMILY,"TestListA");
+        listManager.addEventToList("TestEventB_A", Category.SKILLS,"TestListA");
+        listManager.deleteEvent("TestEventA_A", "TestListA");
+        listManager.deleteEvent("TestEventB_A", "TestListA");
+
+        ListManager compareListManager = new ListManager();
+        compareListManager.createEventlist("TestListA",18,8,2022);
+        compareListManager.createEventlist("TestListB");
+
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+    }
+
+    @Test
+    @DisplayName("testCompleteEvent should work")
+    void testCompleteEvent() {
+        listManager.createEventlist("TestListA",18,8,2022);
+        listManager.createEventlist("TestListB");
+        listManager.addEventToList("TestEventA_A", Category.FAMILY,"TestListA");
+        listManager.addEventToList("TestEventB_A", Category.SKILLS,"TestListA");
+        listManager.completeEvent("TestEventB_A", "TestListA", "...", "The slow purple fox", 5,12,2020);
+
+        ListManager compareListManager = new ListManager();
+        compareListManager.createEventlist("TestListA",18,8,2022);
+        compareListManager.createEventlist("TestListB");
+        compareListManager.addEventToList("TestEventA_A", Category.FAMILY,"TestListA");
+        compareListManager.addEventToList("TestEventB_A", Category.SKILLS,"TestListA");
+        compareListManager.completeEvent("TestEventB_A", "TestListA", "...", "The slow purple fox", 5,12,2020);
+
+        assertEquals(compareListManager.getEventlists().toString(), listManager.getEventlists().toString());
+    }
+
+    /*
+    @Test
+    void testSave() {
+    }
+
+    @Test
+    void testLoad() {
+    }
+
+    @Test
+    void testGetEventlists() {
+    }
+
+     */
+}
---
+++ src/test/java/de/hdmstuttgart/mi/bucketlist/ViewController/CategoryManagerTest.java
@@ -0,0 +1,68 @@
+package de.hdmstuttgart.mi.bucketlist.ViewController;
+
+import de.hdmstuttgart.mi.bucketlist.Model.Category;
+import de.hdmstuttgart.mi.bucketlist.ModelController.ListManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryManagerTest {
+    private CategoryManager categoryManager;
+
+
+    @BeforeEach
+    public void prepareDummyFiles(){
+        ListManager listManager = new ListManager();
+        categoryManager = new CategoryManager(listManager);
+
+        listManager.createEventlist("TestCat1");
+        listManager.createEventlist("TestCat2");
+        listManager.createEventlist("TestCat3",4,6,2021);
+
+        listManager.addEventToList("SKILLS", Category.SKILLS,"TestCat1");
+        listManager.addEventToList("TRAVEL",Category.TRAVEL,"TestCat1");
+        listManager.addEventToList("CULTURE",Category.CULTURE,"TestCat1");
+        listManager.addEventToList("SHOPPING",Category.SHOPPING,"TestCat1");
+
+        listManager.addEventToList("LIFEGOALS",Category.LIFEGOALS,"TestCat2");
+        listManager.addEventToList("CULINARY",Category.CULINARY,"TestCat2");
+        listManager.addEventToList("EDUCATION",Category.EDUCATION,"TestCat2");
+        listManager.addEventToList("SPORT",Category.SPORT,"TestCat2");
+
+        listManager.addEventToList("HOBBY",Category.HOBBY,"TestCat3");
+        listManager.addEventToList("FAMILY",Category.FAMILY,"TestCat3");
+        listManager.addEventToList("RELATIONSHIP",Category.RELATIONSHIP,"TestCat3");
+        listManager.addEventToList("FRIENDS",Category.FRIENDS,"TestCat3");
+
+    }
+
+    @Test
+    @DisplayName("testGetFilledCatgeoryLists should work")
+    void testGetFilledCatgeoryLists() {
+        ListManager compareListManager = new ListManager();
+        CategoryManager compareCategoryManager= new CategoryManager(compareListManager);
+
+        compareListManager.createEventlist("TestCat1");
+        compareListManager.createEventlist("TestCat2");
+        compareListManager.createEventlist("TestCat3",4,6,2021);
+
+        compareListManager.addEventToList("SKILLS", Category.SKILLS,"TestCat1");
+        compareListManager.addEventToList("TRAVEL",Category.TRAVEL,"TestCat1");
+        compareListManager.addEventToList("CULTURE",Category.CULTURE,"TestCat1");
+        compareListManager.addEventToList("SHOPPING",Category.SHOPPING,"TestCat1");
+
+        compareListManager.addEventToList("LIFEGOALS",Category.LIFEGOALS,"TestCat2");
+        compareListManager.addEventToList("CULINARY",Category.CULINARY,"TestCat2");
+        compareListManager.addEventToList("EDUCATION",Category.EDUCATION,"TestCat2");
+        compareListManager.addEventToList("SPORT",Category.SPORT,"TestCat2");
+
+        compareListManager.addEventToList("HOBBY",Category.HOBBY,"TestCat3");
+        compareListManager.addEventToList("FAMILY",Category.FAMILY,"TestCat3");
+        compareListManager.addEventToList("RELATIONSHIP",Category.RELATIONSHIP,"TestCat3");
+        compareListManager.addEventToList("FRIENDS",Category.FRIENDS,"TestCat3");
+
+        assertEquals(compareCategoryManager.getFilledCatgeoryLists().toString(),categoryManager.getFilledCatgeoryLists().toString());
+    }
+}
