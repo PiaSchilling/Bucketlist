35491c80ec28e0fefa2be923c734f425e0a41945
Sara Tietze <st093@hdm-stuttgart.de>
2021-06-23 00:03:10 +0200

    Statistic Gui
    - neue Scene angelegt
    - Progress - Progress Bar fertig implementiert
    - Time - Progress Bar im Scene Builder angelegt, noch ohne funktion
    - Labels gesetzt
    - Labels, die nicht konstitent sind, mit einer fx:id versehen
    - .css angepasst für die Progress Bars
    - in Statistic Manager Methode calculatePercentageCompletedEventsPerListAsDouble() angelegt, da nützlicher als die Methode calculatePercentageCompletedEventsPerList() in der Klasse. Überlegung (in todo aufgenommen): Ist calculatePercentageCompletedEventsPerListAsDouble() redundant?
    - von Scene StatisticEventlist kommt man nun auch ZURÜCK zu Statistic Scene

--- src/main/java/de/hdmstuttgart/mi/bucketlist/Gui/Controller/CustomNodesController/StatisticListsController.java
+++ src/main/java/de/hdmstuttgart/mi/bucketlist/Gui/Controller/CustomNodesController/StatisticListsController.java
@@ -1,9 +1,12 @@
 package de.hdmstuttgart.mi.bucketlist.Gui.Controller.CustomNodesController;
 
+import de.hdmstuttgart.mi.bucketlist.Gui.Controller.SceneController.PaneLoader;
 import de.hdmstuttgart.mi.bucketlist.Gui.Controller.SceneController.StatisticController;
+import de.hdmstuttgart.mi.bucketlist.Gui.Controller.SceneController.StatisticEventlistController;
 import de.hdmstuttgart.mi.bucketlist.ModelController.ListManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,5 +34,12 @@ public class StatisticListsController {
         log.debug("setEventlistNameLabel() for a statistic list has started ");
         this.eventlistName = eventlistName;
         this.eventlistNameLabel.setText(eventlistName.toUpperCase());
+    }
+
+    @FXML
+    public void switchToStatisticListScene() {
+        StatisticEventlistController statisticEventlistController= new StatisticEventlistController(this.listManager, this.eventlistName,this.borderPane);
+        AnchorPane anchorPane = PaneLoader.loadAnchorPane(statisticEventlistController,"statisticEventlist");
+        this.borderPane.setCenter(anchorPane);
     }
 }
